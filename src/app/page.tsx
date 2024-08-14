189ebbcabffc5a419a5d8f8a8de524ec6ac185db
FALAK92 <naaz92falak@gmail.com>
2024-08-14 12:56:53 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="h-screen just flex justify-center items-center bg-pink-950 text-yellow-400">
       <h1 className="uppercase font-extrabold text-8xl border-b-4 border-yellow-400 p-5 animate-pulse
-">"hello  world"</h1>
+">"hello world "</h1>
     </div>
     
   );
